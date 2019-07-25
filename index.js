4780a7004b62df6cb43378372f9688ea9e6d35a1
Alexander Riccio <test35965+learn@gmail.com>
2019-07-25 15:14:08 -0400

    no

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(text) {
+    let newText = text[0].toUpperCase() + text.slice(1);
+    // newtext.append()
+    return newText;
+  }
+
+  static sanitize(text) {
+    let reg = new RegExp("[\W^\(-)'\s]*")
+    let newText = text.replace(reg, '');
+    return newText;
+  }
 }
