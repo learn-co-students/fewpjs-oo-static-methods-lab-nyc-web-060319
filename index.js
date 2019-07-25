b61b4d07f31dabf05fca7ee8906873eab35225b2
te68 <te68@cornell.edu>
2019-07-25 15:49:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(string){
+    let array = string.split(" ")
+    array[0] = Formatter.capitalize(array[0])
+    let newArray = []
+    array.forEach(function(word){
+      if (word != 'a' && word != 'an' && word != 'but' && word != 'of'&& word != 'and' && word != 'for' && word != 'at' && word != 'by' && word != 'from'  && word != 'the')
+      {
+        newArray.push(Formatter.capitalize(word))
+      }
+      else {
+        newArray.push(word)
+      }
+    })
+    return newArray.join(" ")
+  }
 }
+
+// let test = "this is a test"
+// console.log(Formatter.titleize(test))
